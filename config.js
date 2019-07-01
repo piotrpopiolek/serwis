4d59d0d6327b4784248c9e86b4617e739d538918
piotrpopiolek <piotr.popiolek93@gmail.com>
2019-07-01 21:23:44 +0200

    Finished project

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  db:
+    "mongodb+srv://pepe:KnOkfZMKmxakKKvm@cluster0-uoeiz.mongodb.net/test?retryWrites=true&w=majority",
+  keySession: ["TWOJKLUCZ"],
+  maxAgeSession: 24 * 60 * 60 * 1000
+};
